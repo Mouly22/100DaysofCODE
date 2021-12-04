2e2e9dcfd899973b16e899e3955f8f9c646b71e9
Umme Abira Azmary Mouly <95493187+Mouly22@users.noreply.github.com>
2021-12-04 15:04:33 +0600

    .

--- Day 1/1.py
+++ Day 1/1.py
@@ -12,3 +12,23 @@ while n> 0:
     print(n)
     n -= 1
 print("STOP")
+#repeated step
+
+name = input()
+handle = open(name)
+count = dict()
+for line in handle:
+     words = line.split()
+     for word in words:
+        count[word] = count.get(word,0) + 1
+
+bigC = None
+bigW = None
+for word,count in count.items():
+    if bigC is None or count> bigC:
+        bigW = word
+        bigC = count
+
+print(bigW, bigC)
+
+
