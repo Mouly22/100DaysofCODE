2acdb260a9df58c55638ea68310e19a8a8608789
Umme Abira Azmary Mouly <95493187+Mouly22@users.noreply.github.com>
2021-12-18 23:59:54 +0600

    more

---
+++ Day 11/1.py
@@ -0,0 +1,38 @@
+import turtle
+scrn = turtle.Screen()
+scrn.bgcolor("lavender")
+arin = turtle.Turtle()
+arin.color("blue")
+arin.forward(100)
+arin.right(90)                    #name.right(90) goes downward
+arin.forward(90)
+
+import turtle
+scrn = turtle.Screen()
+arin = turtle.Turtle()
+arin.color("hot pink")
+arin.forward(100)
+arin.left(90)                    #name.left(90) goes upward
+arin.forward(90)
+
+#name.right(value)/name.left(value) works for defining angles(degrees).
+
+import turtle
+scrn = turtle.Screen()
+prity = turtle.Turtle()
+prity.color("green")
+prity.right(45)
+prity.forward(60)
+prity.left(90)
+prity.forward(100)
+
+
+
+
+
+
+
+
+
+
+
