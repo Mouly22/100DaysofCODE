8516e3b46ebd03e63e654fa37b073e5059f59c81
Umme Abira Azmary Mouly <95493187+Mouly22@users.noreply.github.com>
2021-12-20 02:56:16 +0600

    loop

--- Day 12/2.py
+++ Day 12/2.py
@@ -1,6 +1,8 @@
+ #If we want to make a repetitive pattern in our drawings, then we can use loops.
+
 import turtle
 scrn = turtle.Screen()
-scrn.bgcolor("Gray")
+scrn.bgcolor("gray")
 mim = turtle.Turtle()
 mim.color("pink")
 mim.pensize(3)
@@ -9,6 +11,18 @@ for i in range(20):
     mim.forward(distance)
     mim.right(90)
     distance = distance + 10
+
+nim = turtle.Turtle()
+nim.color("yellow")
+nim.pensize(2)
+distance = 9
+angle = 90
+for we in range(15):
+    nim.forward(distance)
+    nim.right(angle)
+    distance = distance + 10
+    angle = angle - 3
+
 
 scrn.exitonclick()
 
