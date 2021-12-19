2d94ad364a5a5c8614f2fec147909cfa1c5dd8b6
Umme Abira Azmary Mouly <95493187+Mouly22@users.noreply.github.com>
2021-12-20 02:05:25 +0600

    tri and rec

---
+++ Day 12/1.py
@@ -0,0 +1,26 @@
+import turtle
+scrn = turtle.Screen()
+scrn.bgcolor("lavender")
+rin = turtle.Turtle()
+rin.color("hotpink")
+rin.pensize(5)
+rin.forward(100)
+rin.left(120)
+rin.forward(100)
+rin.left(120)
+rin.forward(100)
+
+rini = turtle.Turtle()
+rini.color("pink")
+rini.pensize(5)
+rini.forward(100)
+rini.left(90)
+rini.forward(100)
+rini.left(90)
+rini.forward(100)
+rini.left(90)
+rini.forward(100)
+
+scrn.exitonclick()
+
+
