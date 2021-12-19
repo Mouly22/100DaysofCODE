6b6052dd5c88040953e5bda3cf460960caaeea6e
Umme Abira Azmary Mouly <95493187+Mouly22@users.noreply.github.com>
2021-12-20 02:56:28 +0600

    know

--- Day 12/1.py
+++ Day 12/1.py
@@ -23,4 +23,9 @@ rini.forward(100)
 
 scrn.exitonclick()
 
-
+#Just like we can have many different integers in a program, we can have many turtles. 
+#Each of the turtle is an independent object and we call each one an instance of the Turtle type (class)
+#Each instance has its own attributes and methods 
+#Geometry conventions have 0 degrees facing East and that is the case here too
+#Each instance can have attributes, sometimes called instance variables
+#alex.forward(50) alex is an instance of the class Turtle.forward is a method.
