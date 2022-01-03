b7ff833f53166a2054d282c43ff55de55939be41
Umme Abira Azmary Mouly <95493187+Mouly22@users.noreply.github.com>
2022-01-04 03:17:40 +0600

    .

---
+++ Day 18/1.py
@@ -0,0 +1,16 @@
+for name in ['rin','zina','sennaa']:
+    print(name.count('n'))
+
+for p in range(10):
+    x = p * p
+    print(x)
+
+for i in "MOULY":
+    if i == "Y":
+        print(i, "Done")
+    else:
+        print(i, 'Next alphabet in next line')
+
+
+
+    
