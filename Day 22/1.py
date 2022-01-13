fb9b92d6f4f565d43f3d04a12529beea34c518a9
Umme Abira Azmary Mouly <95493187+Mouly22@users.noreply.github.com>
2022-01-14 00:02:36 +0600

    up

--- Day 22/1.py
+++ Day 22/1.py
@@ -1,6 +1,6 @@
 #take a input dict from the user
 n = 2
-d = dict(input("Enter: ").split() for i in range(n))        #while taking input write one keu-value in only line and go to the next line
+d = dict(input("Enter: ").split() for i in range(n))        #while taking input write one key-value in only line and go to the next line
 print(d)
 
 #or,
@@ -27,4 +27,3 @@ for i in range(n):
     text = input().split()     #split the input text based on space & store in the list 'text'
     d[text[0]] = text[1]       #assign the 1st item to key and 2nd item to value of the dictionary
 print(d)
-
