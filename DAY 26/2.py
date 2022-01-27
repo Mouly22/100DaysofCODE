fb67a10a5740feb1a6c3e65c11221b15e9fc1f5b
Mouly <abiraazmary22@gmail.com>
2022-01-27 12:36:38 +0600

    ...

--- DAY 26/2.py
+++ DAY 26/2.py
@@ -1,1 +1,7 @@
-print('mutable')
+#convert into binary number
+print(bin(23))                                 #0b represents binary.
+print(0b1111)                                  #it converts from binary into integer
+print(oct(23))
+print(0o27)
+print(hex(10))
+print(0xa)
