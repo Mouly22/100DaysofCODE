4ebcfb6e1eb533125ac90a42fcc065b9da55c42e
Mouly <abiraazmary22@gmail.com>
2022-02-06 15:46:33 +0600

    mmmm

---
+++ Day 30/2.py
@@ -0,0 +1,8 @@
+
+def fun(x, y):
+    sum = x + y 
+    mul = x * y
+    return sum, mul
+
+res1, res2 = fun(3, 10)
+print(res1, res2)
---
+++ Day 30/3.py
@@ -0,0 +1,10 @@
+def up(x):
+    print(x)
+
+up(10)
+
+def down(p):
+    p = 4
+    print(p)
+
+down(2)
