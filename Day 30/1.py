2b40afc445de122ff1116f56b3656a4e9e3e84da
Mouly <abiraazmary22@gmail.com>
2022-02-06 16:22:30 +0600

    fun

--- Day 30/1.py
+++ Day 30/1.py
@@ -2,3 +2,22 @@ def greet():
     print("Hola")
 
 greet()
+
+def fun(x, y):
+    sum = x + y 
+    mul = x * y
+    return sum, mul
+
+res1, res2 = fun(3, 10)
+print(res1, res2)
+
+def up(x):
+    print(x)
+
+up(10)
+
+def down(p):
+    p = 4
+    print(p)
+
+down(2)
