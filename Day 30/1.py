191131fbe029cca7abf48a98e3de7ce26affcccd
Mouly <abiraazmary22@gmail.com>
2022-02-07 04:15:18 +0600

    ...

--- Day 30/1.py
+++ Day 30/1.py
@@ -1,15 +1,15 @@
-def greet():
-    print("Hola")
+def greet():              #define a function
+    print("Hola")     
 
-greet()
+greet()                  #call a function
 
-def fun(x, y):
+def fun(x, y):           #parameter 
     sum = x + y 
     mul = x * y
-    return sum, mul
+    return sum, mul            #I am returning two values 
 
-res1, res2 = fun(3, 10)
-print(res1, res2)
+res1, res2 = fun(3, 10)        #As I am returning two values, I have to accept two values with two variables.
+print(res1, res2)             
 
 def up(x):
     print(x)
--- Day 30/2.py
+++ Day 30/2.py
@@ -1,8 +1,2 @@
 
-def fun(x, y):
-    sum = x + y 
-    mul = x * y
-    return sum, mul
 
-res1, res2 = fun(3, 10)
-print(res1, res2)
